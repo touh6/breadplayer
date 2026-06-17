ee574bb46cac84718db7a8dd4e5968ef3e3bc291
touh <181862795+touh6@users.noreply.github.com>
2026-06-17 20:20:47 +0530

    Update main.cpp

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,1 +1,78 @@
+#include <Arudino.h>
 
+#include "storage.h"
+#include "audio.h"
+#include "display.h"
+#include "buttons.h"
+#include "battery.h"
+
+void setup()
+{
+
+Serial.begin(
+115200
+);
+
+storageBegin();
+
+scanMusic();
+
+displayBegin();
+
+audioBegin();
+
+buttonsBegin();
+
+if(
+  getSongs()>0
+  )
+{
+
+playCurrentSong();
+
+updateScreen(
+
+getCurrentSong(),
+
+getCurrentArt(),
+
+batteryPercent(),
+
+0
+
+  );
+
+}
+
+void loop()
+{
+
+audioLoop();
+
+checkButtons();
+
+static unsigned long t=0;
+
+if(
+  millis()-t > 30000
+  )
+{
+
+updateScreen(
+
+getCurrentSong(),
+
+getCurrentArt(),
+
+batteryPercent(),
+
+getElapsed()
+
+);
+
+t=millis();
+
+}
+
+
+}
