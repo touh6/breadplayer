3bfa0ed8197b24604183cd924b61b6037ed3d327
touh <181862795+touh6@users.noreply.github.com>
2026-06-17 20:05:01 +0530

    Update buttons.cpp

--- firmware/src/buttons.cpp
+++ firmware/src/buttons.cpp
@@ -1,1 +1,70 @@
+#include "buttons.h"
 
+#include "config.h"
+
+#include "audio.h"
+
+#include "storage.h"
+
+void buttonsBegin()
+{
+
+pinMode(
+BUTTON_PLAY,
+INPUT_PULLUP
+);
+
+pinMode(
+BUTTON_NEXT, 
+INPUT_PULLUP
+);
+
+pinMode(
+BUTTON_PREV
+INPUT_PULLIP
+); 
+
+}
+
+void checkButtons()
+{
+
+if(
+  digitalRead(BUTTON_PLAY)==LOW
+  )
+{
+
+togglePause();
+
+delay(300);
+
+}
+
+if(
+  digitalRead(BUTTON_NEXT)==LOW
+  )
+{
+
+nextSong();
+
+playCurrentSong();
+
+delay(300);
+
+}
+
+if(
+  digitalRead(BUTTON_PREV)==LOW
+  )
+{
+
+previousSong();
+
+playCurrentSong();
+
+delay(300); 
+
+}
+
+
+}
