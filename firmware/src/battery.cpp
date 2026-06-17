4b9d5f7ae6199b7388f2454d15e8c9e24ba36775
touh <181862795+touh6@users.noreply.github.com>
2026-06-17 20:10:30 +0530

    Update battery.cpp

--- firmware/src/battery.cpp
+++ firmware/src/battery.cpp
@@ -1,1 +1,32 @@
+#include "battery.h"
 
+#include "config.h"
+
+int batteryPercent()
+{
+
+int value =
+analogRead(
+BATTERY_PIN
+);
+
+float voltage =
+value * 3.3/ 4095 * 2;
+
+
+int percent =
+map(
+voltage*100,
+330,
+420, 
+0, 
+100
+);
+
+return constrain(
+  percent, 
+  0, 
+  100
+  );
+
+}
