d28826a5ddee18efd818a4afb78f214c354bb620
touh <181862795+touh6@users.noreply.github.com>
2026-06-17 18:26:28 +0530

    Update storage.h

--- firmware/include/storage.h
+++ firmware/include/storage.h
@@ -1,1 +1,18 @@
+#pragma once
+
+#include <Arduino.h>
+
+void storageBegin();
+
+void scanMusic();
+
+String getCurrentSong();
+
+String getCurrentArt();
+
+int getSongs();
+
+void nextSong();
+
+void previousSong();
 
