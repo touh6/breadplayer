f4a0770815467463433a7befa9437a0ff51a3e05
touh <181862795+touh6@users.noreply.github.com>
2026-06-17 17:48:01 +0530

    Update config.h

--- firmware/include/config.h
+++ firmware/include/config.h
@@ -1,1 +1,28 @@
 #pragma once
+
+// Waveshare e-Ink
+
+#define EPO_CS 15
+#define EPO_DC 27
+#define EPO_RST 26
+#define EPO_BUSY 25
+
+// 3.3V MicroSD Module
+
+#define SD_CS 5
+
+// PCM5102A
+
+#define I2S_BCLK 32
+#define I2S_LRCK 33
+#define I2S_DATA 22
+
+// Buttons 
+
+#define BUTTON_PLAY 21
+#define BUTTON_NEXT 19
+#define BUTTON_PREV 18
+
+// Battery
+
+#define BATTERY_PIN 34
